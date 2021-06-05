3b0c709b9014a416369b2c373299789162a2306e
unknown <carlos.viniciusufrpe@gmail.com>
2021-06-05 14:35:52 -0300

    definir aleatoriedade da comida e tamanho da cobrinha

--- script.js
+++ script.js
@@ -27,23 +27,23 @@ function criarCobrinha(){
 }
 
 function drawFood(){
-    context.fillStyle = "red"
+    context.fillStyle = "red";
     context.fillRect(food.x, food.y, box, box);
 }
 
 document.addEventListener('keydown', update);
 
 function update(event){
-    if(event.keyCode == 37 && direction != "right") direction = "left";
-    if(event.keyCode == 38 && direction != "down") direction = "up";
-    if(event.keyCode == 39 && direction != "left") direction = "right";
-    if(event.keyCode == 40 && direction != "up") direction = "down";
+    if(event.keyCode == 37 && direction != 'right') direction = 'left';
+    if(event.keyCode == 38 && direction != 'down') direction = 'up';
+    if(event.keyCode == 39 && direction != 'left') direction = 'right';
+    if(event.keyCode == 40 && direction != 'up') direction = 'down';
 }
 function iniciarJogo(){
-    if (snake[0].x > 15 * box && direction == "right") snake[0].x = 0;
-    if(snake[0].x < 15 * box && direction == "left") snake[0].x = 16 * box;
-    if(snake[0].y > 15 * box && direction == "down") snake[0].y = 0;
-    if(snake[0].y < 0 && direction == "up") snake[0].y = 16 * box;
+    if (snake[0].x > 15 * box && direction == 'right') snake[0].x = 0;
+    if(snake[0].x < 15 * box && direction == 'left') snake[0].x = 16 * box;
+    if(snake[0].y > 15 * box && direction == 'down') snake[0].y = 0;
+    if(snake[0].y < 0 && direction == 'up') snake[0].y = 16 * box;
 
     criarBG();
     criarCobrinha();
@@ -57,8 +57,13 @@ function iniciarJogo(){
     if(direction == "up") snakeY -= box;
     if(direction == "down") snakeY += box;
 
-    snake.pop();
-
+    if(snakeX != food.x || snakeY != food.y){
+        snake.pop();
+    }
+    else{
+        food.x = Math.floor(Math.random() * 15 + 1) * box;
+        food.y = Math.floor(Math.random() * 15 + 1) * box;
+    }
     let newHead = {
         x: snakeX,
         y: snakeY
